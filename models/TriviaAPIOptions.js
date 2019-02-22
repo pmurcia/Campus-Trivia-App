fa609d6ccc8b5ae6b081a9f16894cc558b3905f0
pmurcia <p.murcia.morilla@gmail.com>
2019-02-22 03:40:02 -0600

    Created project & OpenTriviaDB handler

---
+++ models/TriviaAPIOptions.js
@@ -0,0 +1,7 @@
+function TriviaAPIOptions(amount, category, difficulty, type) {
+    this.amount = amount
+    this.category = category
+    this.difficulty = difficulty
+    this.type = type
+    this.encode = 'base64'
+}
---
+++ models/TriviaQuestion.js
@@ -0,0 +1,8 @@
+function TriviaQuestion(category,type,difficulty,question,answers,correct_answer) {
+    this.category = category
+    this.type = type
+    this.difficulty = difficulty
+    this.question = question
+    this.answers = answers
+    this.correct_answer = correct_answer
+}

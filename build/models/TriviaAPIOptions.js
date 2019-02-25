208cf768c7829718cddefef14944272c239c691a
pmurcia <p.murcia.morilla@gmail.com>
2019-02-25 12:16:53 -0600

    Removed build folder

--- build/models/TriviaAPIOptions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TriviaAPIOptions = /** @class */ (function () {
-    function TriviaAPIOptions(amount, category, difficulty, type) {
-    }
-    return TriviaAPIOptions;
-}());
-exports.TriviaAPIOptions = TriviaAPIOptions;

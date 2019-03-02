07801506adf133d8877b4f558ef79fc6d94e8333
pmurcia <p.murcia.morilla@gmail.com>
2019-03-01 21:37:36 -0600

    Preparing to create user handler

--- app.ts
+++ app.ts
@@ -1,8 +1,6 @@
 import express = require('express');
-//import https = require('https');
 
 import { OpenTriviaDB } from './util/OpenTriviaDB';
-//import { TriviaCategory, TriviaQuestion } from './models/TriviaQuestion';
 
 const PORT = process.env.PORT || '5000';
 const app: express.Application = express();
@@ -12,7 +10,7 @@ app.get('/', (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/json; charset=utf-8');
     OpenTriviaDB.getQuestions(5).then((questions) => {
-        console.log(questions);
+        //console.log(questions);
         let mapped = questions.map(q => q.toAppInventor())
         res.end(JSON.stringify(mapped));
     }).catch(err => {
--- build/app.js
+++ build/app.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
-//import https = require('https');
 var OpenTriviaDB_1 = require("./util/OpenTriviaDB");
-//import { TriviaCategory, TriviaQuestion } from './models/TriviaQuestion';
 var PORT = process.env.PORT || '5000';
 var app = express();
 console.log(PORT);
@@ -11,7 +9,7 @@ app.get('/', function (req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/json; charset=utf-8');
     OpenTriviaDB_1.OpenTriviaDB.getQuestions(5).then(function (questions) {
-        console.log(questions);
+        //console.log(questions);
         var mapped = questions.map(function (q) { return q.toAppInventor(); });
         res.end(JSON.stringify(mapped));
     }).catch(function (err) {
